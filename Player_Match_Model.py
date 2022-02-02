a51b69f0e60bcec5335646bd459bf690abb699fd
ChazND <78455942+ChazND@users.noreply.github.com>
2022-02-02 16:14:55 -0500

    Add files via upload

--- Player_Match_Model.py
+++ Player_Match_Model.py
@@ -1,10 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib
-matplotlib.use("Agg")
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_agg import RendererAgg
-from matplotlib.figure import Figure
 import seaborn as sns
 import statsmodels
 from statsmodels.nonparametric.smoothers_lowess import lowess
